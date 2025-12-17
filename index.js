7b5f8f565594f16840d65d0ce639e877979da1c3
Addison Bert <bertar23@juniata.edu>
2025-12-17 17:18:24 +0000

    yipeeee

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ document.getElementById('genreForm').addEventListener('submit', function () {
       const input = document.getElementById('favoriteGenre').value
 
       if (input === 'alternative') {
-        alert("that's my favorite");
+        alert("That's my favorite!");
       } else {
-        alert('you have good taste');
+        alert('You have good taste!');
       }
     });
 
-const genre = document.querySelector("#genre");
-genre.oninput = function (){
-    h2.innerText = "I Love " + genre.value;
-};
+
    
